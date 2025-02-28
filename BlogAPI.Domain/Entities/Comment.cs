057a3c1ae6e0735a515cf573e0214c8721daef06
Sametcl <samet2016.2022@gmail.com>
2025-02-28 15:13:14 +0300

    Add project files.

---
+++ BlogAPI.Domain/Entities/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAPI.Domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public DateTime CreadetAt { get; set; } = DateTime.UtcNow;
+
+        //Foreign Key   
+        public int PostId { get; set; }
+        public Post? Post { get; set; }
+
+
+    }
+}
---
+++ BlogAPI.Domain/Entities/Post.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogAPI.Domain.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }=string.Empty;
+        public string Content { get; set; }=string.Empty;
+        public DateTime CreatedAt { get; set; }=DateTime.UtcNow;
+
+        //Post category 1-n 
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        //Post Comment  1-n
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
